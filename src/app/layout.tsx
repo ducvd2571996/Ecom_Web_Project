6f3d43c134c2d7118b80601abbc38750b37c88c1
GNAUQ153 <151826167+GNAUQ153@users.noreply.github.com>
2024-11-09 21:18:06 +0700

    1st update comment code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
-import Footer from '@/app/components/footer/Footer';
 import Header from '@/app/components/header/Header';
+import Footer from '@/app/components/footer/Footer';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import { Poppins } from 'next/font/google'; // Import Poppins instead of Roboto
@@ -24,14 +24,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
+  const pathname = usePathname(); // Lấy đường dẫn hiện tại của ứng dụng.
+  // Kiểm tra xem có phải là trang đăng nhập hoặc đăng ký không.
   const isLoginPage = pathname === '/login' || pathname === '/register';
   return (
     <html lang="en">
       <body className={poppins.variable}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            <Provider store={store}>
+          <ThemeProvider theme={theme}> 
+            <Provider store={store}> 
+              {/* Nếu không phải là trang đăng nhập hoặc đăng ký thì hiển thị Header và NavBar */}
               {!isLoginPage &&
               <Box>
                 <Header />
@@ -39,6 +41,7 @@ export default function RootLayout({
               </Box>
                }
               <main>{children}</main>
+              {/* Nếu không phải là trang đăng nhập hoặc đăng ký thì hiển thị Footer */}
               {!isLoginPage && <Footer />}
             </Provider>
           </ThemeProvider>
